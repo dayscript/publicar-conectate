bc3ae3c84d8c2b6a1c857e14542f7e2b1b05bc09
jcardenas2015 <jcardenas@dayscript.com>
2017-08-23 12:35:49 -0500

    new theme sumate

---
+++ sites/all/themes/sumate/js/personal.js
@@ -0,0 +1,152 @@
+(function($){
+ 	$(document).ready(function()
+ 	{
+        var URLactual = window.location.hostname+""; 
+
+        if ((window.location.pathname == '/' || window.location.pathname == '/node') && document.getElementById("usuario_id") == null) 
+        {
+            if (typeof $(".first") !== 'undefined') {
+              //$(".first")[0].outerHTML = '<li class="first last" style="font-size: 10px;">usted acepta los <a href="http://conectatepublicar.linkpruebas.com/sites/default/files/conectate-terminos.pdf" title="" target="_blank">Términos y Condiciones</a> y la <a href="http://conectatepublicar.web/sites/default/files/politicadeprivacidad.pdf" target="_blank" title="">Política de Privacidad</a> del Programa "Conéctate Publicar"</li>';
+              $(".first")[0].outerHTML = '';
+            }
+        }
+        if (document.getElementById("administrarimagen") != null) {
+          $(".sec-que-es").removeClass('sec-que-es');
+          $(".block-title")[1].style.display = 'none';
+          //$(".sec-que-es")[0].style.removeProperty("background-image");
+        }
+ 		
+ 		var d = new Date();
+ 		var data = {
+            documento: document.getElementById("usuario_id").value,
+            dia : d.getDate(),
+            mes : d.getMonth()+1,
+            ano : d.getFullYear()
+        };
+ 		$.ajax({
+            url: "http://"+URLactual+ "/conect/index.php/admin/job/puntosPorUsuario",
+            type: 'post',
+            data: data,
+            success: function(info){
+            	modResponse = $.parseJSON(info);
+                if (modResponse.estado=== false) {
+                    for (i = 0; i < $(".puntosTotal").length; i++) {
+					    $(".puntosTotal")[i].textContent = 0;
+					}
+                }else{
+                    for (i = 0; i < $(".puntosTotal").length; i++) {
+					    $(".puntosTotal")[i].textContent = modResponse.carga;
+					} 
+                }
+            }       
+        });
+        $.ajax({
+            url: "http://"+URLactual+ "/conect/index.php/admin/job/metasPorUsuario/1",
+            type: 'post',
+            data: data,
+            success: function(info){
+                modResponse = $.parseJSON(info);
+                if (modResponse.estado=== false) {
+                    document.getElementById("rendimiento").outerHTML = '<div id="rendimiento">'+modResponse.carga+'</div>';
+                }else{
+                    for (i = 0; i < $("#rendimiento").length; i++) {
+                        $("#rendimiento")[i].outerHTML = '<div id="rendimiento">'+modResponse.carga+'</div>';
+                    }
+                }
+            }       
+        });
+        function cargarmetodo()
+        {
+            $( ".mes" ).change(function(ev) {
+                data.mes = ev.currentTarget[ev.currentTarget.selectedIndex].value;
+                $.ajax({
+                    url: "http://"+URLactual+ "/conect/index.php/admin/job/metasPorUsuario",
+                    type: 'post',
+                    data: data,
+                    success: function(info){
+                        modResponse = $.parseJSON(info);
+                        if (modResponse.estado=== false) {
+                            document.getElementById("rendimientoTotal").outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                        }else{
+                            for (i = 0; i < $(".rendimientoTotal").length; i++) {
+                                $(".rendimientoTotal")[i].outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                            }
+                        }
+                        cargarmetodo();
+                    }     
+                });
+            });
+        }
+
+        $.ajax({
+            url: "http://"+URLactual+ "/conect/index.php/admin/job/metasPorUsuario",
+            type: 'post',
+            data: data,
+            success: function(info){
+                modResponse = $.parseJSON(info);
+                if (modResponse.estado=== false) {
+                    document.getElementById("rendimientoTotal").outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                }else{
+                    for (i = 0; i < $(".rendimientoTotal").length; i++) {
+                        $(".rendimientoTotal")[i].outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                    }
+                }
+                cargarmetodo();
+            }     
+        });
+        document.getElementById("quicktabs-tab-tabs_rendimiento-1").onclick = function()
+        {
+           $.ajax({
+                url: "http://"+URLactual+ "/conect/index.php/admin/job/rankingxgrupo",
+                type: 'post',
+                data: data,
+                success: function(info){
+                    modResponse = $.parseJSON(info);
+                    if (modResponse.estado=== false) {
+                        document.getElementById("rendimientoRanking").outerHTML = '<div id="rendimientoRanking">'+modResponse.carga+'</div>';
+                    }else{
+                        for (i = 0; i < $(".rendimientoRanking").length; i++) {
+                            $(".rendimientoRanking")[i].outerHTML = '<div id="rendimientoRanking" class="rendimientoRanking">'+modResponse.carga+'</div>';
+                        }
+                        for (i = 0; i < $(".rendimientoRanking").length; i++) {
+                            $(".rendimientoRanking")[i].outerHTML = '<div id="rendimientoRanking" class="rendimientoRanking">'+modResponse.carga+'</div>';
+                        }
+                    }
+                }       
+            });
+        }
+	}); 
+    
+})(jQuery);
+
+
+/*
+    function cambioMes()
+    {
+        preuba();
+        /*
+        var d = new Date();
+        var data = {
+            documento: document.getElementById("usuario_id").value,
+            dia : d.getDate(),
+            mes : d.getMonth()+1,
+            ano : d.getFullYear()
+        };
+        $.ajax({
+            url: "http://"+URLactual+ "/conect/index.php/admin/job/metasPorUsuario",
+            type: 'post',
+            data: data,
+            success: function(info){
+                modResponse = $.parseJSON(info);
+                if (modResponse.estado=== false) {
+                    document.getElementById("rendimientoTotal").outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                }else{
+                    for (i = 0; i < $(".rendimientoTotal").length; i++) {
+                        $(".rendimientoTotal")[i].outerHTML = '<div id="rendimientoTotal">'+modResponse.carga+'</div>';
+                    }
+                }
+            }       
+        });
+        
+    }
+    */
---
+++ sites/all/themes/sumate/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function conectate_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function conectate_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function conectate_preprocess_node(&$variables) {
+}
