181a4a380233e861188f3fdc0279db9758732ce4
aacevedoday <aacevedo@dayscript.com>
2017-08-28 15:55:26 -0500

    Domain Access

--- sites/all/themes/sumate/template.php
+++ sites/all/themes/sumate/template.php
@@ -3,17 +3,17 @@
 /**
  * Implements template_preprocess_html().
  */
-function conectate_preprocess_html(&$variables) {
+function sumate_preprocess_html(&$variables) {
 }
 
 /**
  * Implements template_preprocess_page.
  */
-function conectate_preprocess_page(&$variables) {
+function sumate_preprocess_page(&$variables) {
 }
 
 /**
  * Implements template_preprocess_node.
  */
-function conectate_preprocess_node(&$variables) {
+function sumate_preprocess_node(&$variables) {
 }
--- sites/all/themes/sumate/theme-settings.php
+++ sites/all/themes/sumate/theme-settings.php
@@ -2,6 +2,6 @@
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function conectate_form_system_theme_settings_alter(&$form, &$form_state) {
+function sumate_form_system_theme_settings_alter(&$form, &$form_state) {
 
 }
