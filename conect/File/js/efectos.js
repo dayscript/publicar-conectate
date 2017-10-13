20033be313ab00c65926f3988c1f1afe8e610098
tecnologiagrupolaestacion <tecnologia@grupolaestacion.com>
2017-10-13 16:07:28 +0000

    carga pendiente

---
+++ conect/File/js/efectos.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $( "#menu" ).click(function() {
+       $('.menu').show(500);
+  });
+    $( "#ocultar" ).click(function() {
+       $('.menu').hide(500);
+  });
+});
